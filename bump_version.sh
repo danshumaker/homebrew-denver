46840a85fbbfaa4f8905548edc8d1d486e6d8257
Dan Shumaker <danpshumaker@gmail.com>
2025-12-10 16:13:40 -0500

    Change version to date

--- bump_version.sh
+++ bump_version.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NEW="$1"
-echo "$NEW" >VERSION
+DATE=$(date "+%Y-%m-%d_%H_%M_%S")
+
+echo "$DATE" >VERSION
 
 # Recompute SHA
-TAR_URL="https://github.com/danshumaker/homebrew-denver/archive/refs/tags/v${NEW}.tar.gz"
+TAR_URL="https://github.com/danshumaker/homebrew-denver/archive/refs/tags/v${DATE}.tar.gz"
 SHA=$(curl -L "$TAR_URL" | shasum -a 256 | awk '{print $1}')
 
 sed -i.bak "s/sha256 \".*\"/sha256 \"${SHA}\"/" Formula/denver.rb
 rm Formula/denver.rb.bak
 
 git add VERSION Formula/denver.rb
-git commit -m "Bump version to v${NEW}"
-git tag "v${NEW}"
+git commit -m "Bump version to ${DATE}"
+git tag "${DATE}"
 git push origin main --tags
