211a1bb831700372a3323b956f27e60deaa688aa
Dan Shumaker <danpshumaker@gmail.com>
2025-12-10 23:28:48 -0500

    Fixing workflows

--- bump_version.sh
+++ bump_version.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+set -xv
 DATE=$(date "+%Y-%m-%d_%H_%M_%S")
 
 echo "$DATE" >VERSION
 
-sed -i.bak "s/version \".*\"/version \"${DATE}\"/" Formula/denver.rb
-# Recompute SHA
-TAR_URL="https://github.com/danshumaker/homebrew-denver/archive/refs/tags/${DATE}.tar.gz"
-SHA=$(curl -L "$TAR_URL" | shasum -a 256 | awk '{print $1}')
-
-sed -i.bak "s/sha256 \".*\"/sha256 \"${SHA}\"/" Formula/denver.rb
-rm Formula/denver.rb.bak
+sed -i "s/version \".*\"/version \"${DATE}\"/" Formula/denver.rb
 
 git add VERSION Formula/denver.rb
 git commit -m "Bump version to ${DATE}"
