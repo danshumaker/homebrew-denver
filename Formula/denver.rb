d65732832d92c99c26a437c7ae5420deeb2b2f16
Dan Shumaker <danpshumaker@gmail.com>
2026-01-05 14:41:05 -0500

    Bump version to 2026-01-05_14_41_03

--- Formula/denver.rb
+++ Formula/denver.rb
@@ -1,7 +1,7 @@
 class Denver < Formula
   desc "denver dotfiles payload for installation and uninstallation scripts"
   homepage "https://github.com/danshumaker/homebrew-denver"
-  version "2026-01-03_11_56_59"
+  version "2026-01-05_14_41_03"
   url "https://codeload.github.com/danshumaker/homebrew-denver/tar.gz/#{version}"
   sha256 "b362c3aebd18a6e3d508b83be0be4941d64a6085732042f970ec6362c9f34ee8"
   license "MIT"
